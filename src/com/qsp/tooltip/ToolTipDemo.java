ad9c9a82e119f8cbda12ae032a89bc7f73d05a24
Aravinda <aru03.info@gmail.com>
2019-05-19 13:24:21 +0530

    19-05-2019-1

---
+++ src/com/qsp/tooltip/ToolTipDemo.java
@@ -0,0 +1,23 @@
+package com.qsp.tooltip;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import com.qsp.utils.ApplicationUtils;
+import com.qsp.utils.DriverUtils;
+
+public class ToolTipDemo 
+{
+
+	public static void main(String[] args) 
+	{
+		WebDriver driver = DriverUtils.getMyDriver();
+		ApplicationUtils.launchApp("https://www.google.com/");
+		
+		WebElement googleImg = driver.findElement(By.xpath("//img[@style='padding-top:51px']"));
+		WebElement appsIcon = driver.findElement(By.xpath("//a[@class='gb_x gb_Ub']"));
+		System.out.println(googleImg.getAttribute("title"));
+		System.out.println(appsIcon.getAttribute("title"));
+	}
+}
